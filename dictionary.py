646518427fc4e80491adce4f71ca29ab65d015da
sanjayvarma-gif <sanjayvarmapernapati@gmail.com>
2026-09-11 10:34:06 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,26 @@
+#sets
+numbers = [10,20,30,40,50,60,70,80]
+
+print(numbers[::-2])
+print(numbers[::-3])
+print(numbers[::-4])
+
+
+#example
+numbers = [10,20,30,40,50,60,70,80]
+print(numbers[1:7:2])
+print(numbers[6:1:-2])
+
+ #time calculator
+total_minutes = int(input("Enter total minutes: "))
+
+hours = total_minutes // 60
+minutes = total_minutes % 60
+seconds = total_minutes % 60
+
+print("Hours:", hours)
+print("Minutes:", minutes)
+print("Seconds:", seconds)
+
+
+# 
